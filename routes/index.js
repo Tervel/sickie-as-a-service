3d5c98052c0b76a2c1185d385966a376763392db
Jordan Anslow <jordananslow@gmail.com>
2015-08-17 22:27:48 +1000

    Added new route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var util = require('util'); // for string formatting
+
 var router = express.Router();
+
+'use strict';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET default illness. */
+router.get('/:name/:illness', function(req, res, next) {
+  var name = util.format("Mate, %s.", req.params.name);
+  var illness = util.format("- %s", req.params.illness);
+  var output = util.format("%s %s", name, illness);
+
+  res.send(output);
 });
 
 module.exports = router;
