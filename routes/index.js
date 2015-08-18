630a4426c5899e1b12f726c01618b351aa13d119
Jordan Anslow <jordananslow@gmail.com>
2015-08-18 23:13:55 +1000

    Added stub example

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,13 @@ router.get('/:name/:illness', function(req, res, next) {
   res.send(output);
 });
 
+/* GET illness stub example. */
+router.get('/days/:name/:illness', function(req, res, next) {
+  var name = util.format("Mate, %s for days.", req.params.name);
+  var illness = util.format("- %s", req.params.illness);
+  var output = util.format("%s %s", name, illness);
+
+  res.send(output);
+});
+
 module.exports = router;
